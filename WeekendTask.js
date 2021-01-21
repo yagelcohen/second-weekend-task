7a0f45e3ff370ee27a78025e98a5025d287c1ec9
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-21 15:04:46 +0200

    start work

---
+++ WeekendTask.js
@@ -0,0 +1,18 @@
+let array = []
+
+array[0] = {
+
+    startedAt: new Date("2021-01-20:13:00"),
+
+    finishedAt: new Date("2021-01-20:19:00"),
+    
+    totalTime: 6,
+
+    tasksGiven: 10,
+    
+    tasksFinished: 7,
+    
+    tasksFinishedPrecent: 70,
+
+    topic: JavaScript
+}
